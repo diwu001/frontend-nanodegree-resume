eb9d644e3ec5d7813b557c1fca11897fdbed2070
Di Wu (Tina) <diwubupt@gmail.com>
2015-08-29 22:27:35 -0700

    feat

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,193 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+// var HTMLheaderName = '<h1 id="name">%data%</h1>';
+// var HTMLheaderRole = '<span>%data%</span><hr/>';
+
+// var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
+// var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+// var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
+// var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
+// var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+// var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
+// var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+
+// var HTMLbioPic = '<img src="%data%" class="biopic">';
+// var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+
+ var bio={
+ 	"name":"Tina D. Wu",
+ 	"role":"Software Developer Intern",
+ 	"contacts": {
+		"mobile": "806-252-9105",
+		"email": "diwubupt@gmail.com",
+		"github": "diwu001",
+        "linkedin": "tinadwu",
+		"location": "Richmond, CA"}
+ };
+
+var welcome_msg = "Welcome to my resume!";
+var bioPic =  "images/tina.jpg";
+
+$("#header").prepend(HTMLheaderRole.replace("%data%", "Software Developer Intern in Trans Intelligence LLC"));
+$("#header").prepend(HTMLheaderName.replace("%data%","Tina D. Wu"));
+$("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile));
+$("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
+$("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
+$("#topContacts").append(HTMLlinkedin.replace("%data%", bio.contacts.linkedin));
+$("#topContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
+
+
+$("#header").append(HTMLbioPic.replace("%data%", bioPic));
+
+$("#header").append(HTMLskillsStart);
+
+var skills = ["JavaScript", "jQuery", "Java", "PHP", "MySQL", "Bootstrap", "Git/GitHub"];
+
+function displaySkill() {	
+	for (var i = 0; i < skills.length; i++) {
+		$("#header").append(HTMLskills.replace("%data%", skills[i]));
+	};
+}
+displaySkill();
+
+var work=[{
+ 	"employer":"Trans Intelligence LLC",
+ 	"title":"Software Developer Intern",
+ 	"location":"San Pablo, CA",
+ 	"dates":"2015-present",
+ 	"description":"Develop a dynamic map-based web application for traffic signal data management and visualization"
+ 	}, 
+ 	{
+ 	"employer":"Texas Tech University",
+ 	"title":"Web Developer",
+ 	"location":"Lubbock, TX",
+ 	"dates":"2013-2014",
+ 	"description":"Designed and developed highly customized and responsive websites for two research centers"
+ 	}, 
+ 	{
+ 	"employer":"China Mobile Communications Cooperation",
+ 	"title":"Network Engineer",
+ 	"location":"Beijing, China",
+ 	"dates":"2011-2013",
+ 	"description":"Performed advanced troubleshooting for core mobile networks in Beijing"
+ 	}
+];
+
+var projects=[{
+ 	"title":"Social Networking Website Development Project",
+ 	"dates":"January 2014 - May 2014",
+ 	"description":"Developed a social networking website using PHP, WordPress and MySQL. Users can register, post articles, create connections and discuss with health experts.",
+ 	"images":[]
+ 	}, 
+ 	{
+ 	"title":"Android development and automated testing",
+ 	"dates":"September 2013 - December 2013",
+ 	"description":"Developed an Android application to perform Matrix Calculations. Devised automated functional, model-based and structural testing using Robotium, Emma and MonkeyRunner. Developed Python scripts to perform stress testing.",
+ 	"images":[]
+ 	}
+ ];
+
+ var education=[{
+ 	"name":"Texas Tech University",
+ 	"location":"Lubbock, TX",
+ 	"degree":"Master",
+ 	"majors":"Computer Science",
+ 	"dates":"2013 - 2014",
+ 	"url":""
+ 	}, 
+ 	{
+ 	"name":"Beijing University of Posts and Telecommunications",
+ 	"location":"Beijing, China",
+ 	"degree":"Master",
+ 	"majors":"Electrical Engineering",
+ 	"dates":"2008 - 2011",
+ 	"url":"",
+ 	}, 
+ 	{
+ 	"name":"China Agricultural University",
+ 	"location":"Beijing, China",
+ 	"degree":"Bachelor",
+ 	"majors":"Electrical Engineering",
+ 	"dates":"2004 - 2008",
+ 	"url":""
+ 	}
+];
+
+function displayWork()
+{
+	for(item in work){
+
+		$("#workExperience").append(HTMLworkStart);
+
+		var Employer = HTMLworkEmployer.replace("%data%", work[item].employer);
+		$(".work-entry:last").append(Employer);
+
+		var title = HTMLworkTitle.replace("%data%", work[item].title);
+		$(".work-entry:last").append(title);
+
+		var dates = HTMLworkDates.replace("%data%", work[item].dates);
+		$(".work-entry:last").append(dates);
+
+		var formatted_location = HTMLworkLocation.replace("%data%", work[item].location);
+		$(".work-entry:last").append(formatted_location);
+
+		var description = HTMLworkDescription.replace("%data%", work[item].description);
+		$(".work-entry:last").append(description);
+	}
+}
+
+displayWork();
+
+projects.display = function() {
+
+	for(var i=0;i<projects.length;i++){
+		$("#projects").append(HTMLprojectStart);
+		var Title = HTMLprojectTitle.replace("%data%", projects[i].title);
+		$(".project-entry:last").append(Title);
+
+		var Dates = HTMLprojectDates.replace("%data%", projects[i].dates);
+		$(".project-entry:last").append(Dates);
+
+		var Description = HTMLprojectDescription.replace("%data%", projects[i].description);
+		$(".project-entry:last").append(Description);
+
+		if (projects[i].images.length>0)
+		{
+			for (var j=0;j<projects[i].images.length;j++)		
+			{				
+				var Formmated_image = HTMLprojectTitle.replace("%data%", projects[i].images);
+				$(".project-entry:last").append(Formmated_image);
+			}	
+		}
+	}
+}
+
+projects.display();
+
+function displayEducation()
+{
+	for(var i=0;i<education.length;i++)
+	{
+		$("#education").append(HTMLschoolStart);
+		var f_name = HTMLschoolName.replace("%data%", education[i].name);
+		$(".education-entry:last").append(f_name);
+
+		var f_degree = HTMLschoolDegree.replace("%data%", education[i].degree);
+		$(".education-entry:last").append(f_degree);
+
+		var f_dates = HTMLschoolDates.replace("%data%", education[i].dates);
+		$(".education-entry:last").append(f_dates);
+
+		var f_location = HTMLschoolLocation.replace("%data%", education[i].location);
+		$(".education-entry:last").append(f_location);
+
+		var f_major = HTMLschoolMajor.replace("%data%", education[i].majors);
+		$(".education-entry:last").append(f_major);
+	}
+}
+
+displayEducation();
+
+$("#mapDiv").append(googleMap);
+
